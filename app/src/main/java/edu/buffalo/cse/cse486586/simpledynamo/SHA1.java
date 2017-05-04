bf69886c27055752786c31e94fd7e873fee01bec
nox <abelogla@buffalo.edu>
2017-05-04 19:23:34 -0400

    Initial commit

---
+++ app/src/main/java/edu/buffalo/cse/cse486586/simpledynamo/SHA1.java
@@ -0,0 +1,27 @@
+package edu.buffalo.cse.cse486586.simpledynamo;
+
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+
+public class SHA1 {
+
+    public static String genHash(String in) {
+        MessageDigest sha1 = null;
+        try {
+            sha1 = MessageDigest.getInstance("SHA-1");
+        }
+        catch (NoSuchAlgorithmException e) {
+            Log.e("SHA1", e.toString());
+        }
+        byte[] sha1Hash = sha1.digest(in.getBytes());
+        Formatter formatter = new Formatter();
+        for (byte b : sha1Hash) {
+            formatter.format("%02x", b);
+        }
+        return formatter.toString();
+    }
+
+}
